74081f7551a81b63f5021895a52df281fca33724
Reryson Farinha <rerysonfarinha@outlook.com>
2024-03-03 15:59:05 -0300

    Dashboard - Entrada de dados

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,14 @@
 import adivinhacao
 import forca
 
+def escolha_jogo():
+    print("|-------------------------|")
+    print("|     Escolha o jogo      |")
+    print("|-------------------------|\n")
+
+    print("1 - Adivinhação\n2 - Forca\n")
+    escolha = int(input("Escolha:  "))
+
+
+if(__name__ == "__main__"):
+    escolha_jogo()
